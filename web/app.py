145f5d965da1d2bd8a06746a66e5556fe0ff4a27
mulugeta-89 <mulugetahail89@gmail.com>
2024-03-20 16:20:40 +0300

    /predict_gdp endpoint implemented

---
+++ web/app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from joblib import load
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Endpoint to accept JSON input and return predicted GDP per capita
+@app.route('/predict_gdp', methods=['POST'])
+def predict_gdp():
+    # Get JSON data from the request
+    data = request.json
+
+    # Extract continent from the data and convert to lowercase
+    continent = data.pop("continent")
+    continent = continent.lower()
+
+    # Load the corresponding trained model based on the continent
+    try:
+        model = load(f"models/trained_predictor_{continent}.joblib")
+    except FileNotFoundError:
+        return "Model not found for the specified continent!"
+
+    # Create a DataFrame from the remaining JSON data
+    new_data = pd.DataFrame.from_dict(data, orient='index', columns=['value']).T
+    
+    # Predict GDP per capita using the loaded model
+    try:
+        predicted_gdp_per_capita = model.predict(new_data)[0]
+    except Exception as e:
+        return f"Prediction failed: {e}"
+
+    # Return predicted GDP per capita as JSON response
+    return jsonify({'predicted_gdp_per_capita': predicted_gdp_per_capita})
+
+if __name__ == '__main__':
+    app.run(debug=True)
